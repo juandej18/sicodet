9bda106ce65bd4cab308832af5fa641e45ee2d88
Juan <Juan@192.168.0.117>
2013-09-25 21:11:48 -0430

    Inicializar proyecto

---
+++ src/main/java/ve/com/fsjv/sicodet/App.java
@@ -0,0 +1,16 @@
+package ve.com.fsjv.sicodet;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        JOptionPane.showMessageDialog(null, "Prueba");
+    }
+}
